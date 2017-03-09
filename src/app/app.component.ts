d8a453c7427f37c32088912cdfe282bf77f15dca
Logan Saso <logan.saso2016@gmail.com>
2017-03-09 01:12:30 -0800

    Moves the pages to components - Changed themeing of title bars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { Page1 } from '../components/page1/page1';
+import { Page2 } from '../components/page2/page2';
 
 
 @Component({
